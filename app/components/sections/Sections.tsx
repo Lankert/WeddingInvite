2ef5fd8fda4a318a21d9e54e4678fa808e98f343
Robin <robin_arturo@hotmail.com>
2026-02-09 22:39:14 -0600

    fix

--- app/components/sections/Sections.tsx
+++ app/components/sections/Sections.tsx
@@ -1,39 +1,22 @@
-import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
-
-const Locations = dynamic(() => import('./Locations'), { ssr: false });
-const Reserva = dynamic(() => import('./Reserva'), { ssr: false });
-const Dresscode = dynamic(() => import('./Dresscode'), { ssr: false });
-const ShowTime = dynamic(() => import('./ShowTime'), { ssr: false });
-const Gifts = dynamic(() => import('./Gifts'), { ssr: false });
-const Galeria = dynamic(() => import('./Galeria'), { ssr: false });
-const Contacto = dynamic(() => import('./Contacto'), { ssr: false });
+import Dresscode from "./Dresscode";
+import ShowTime from "./ShowTime";
+import Reserva from "./Reserva";
+import Contacto from "./Contacto";
+import Locations from "./Locations";
+import Gifts from "./Gifts";
+import Galeria from "./Galeria";
 
 const Sections = () => {
     return (
         <div>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Locations />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Reserva />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Dresscode />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-                <ShowTime />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Gifts />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Galeria />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Contacto />
-            </Suspense>
-        </div>
+            <Locations />
+            <Reserva />
+            <Dresscode />
+            <ShowTime />
+            <Gifts />
+            <Galeria />
+            <Contacto />
+         </div>
     );
 }
 
